430c20618316b54d7c49ed4fbb9c8a180b5627e7
R-Umemura <e5a201163d@edu.teu.ac.jp>
2021-10-28 00:57:06 +0900

    AMT203_2021/10/27

--- AMT203/AMT203.ino
+++ AMT203/AMT203.ino
@@ -1,14 +1,15 @@
 // GR-PEACHボードを使ったプログラムです
-// このプログラムは，AMT203を読み取るものです．
+// このプログラムは，AMT203を読み取るまたは原点をリセットするものです．
 // uenosuke(上野先生)が作成したライブラリの使用を前提としています．
 // 作成日 2021/05/22
+// 最終更新日 2021/10/27
 // 作成者 Rumemura(梅村留奈)
 
 //メモ
 //AMT203値を初期化＆読み取れるプログラムです．
 //初期化のときだいたい4096と出力されますがそれで大丈夫です．
 //２つ同時に読めるようになっています．
-
+//AMT203のCSBピンやスイッチのピン番号はdefineの方から変えてください
 
 #include <Arduino.h>
 #include <MsTimer2.h>
@@ -24,9 +25,11 @@ bool flag_10ms = false; // loop関数で10msごとにシリアルプリントで
 bool flag_100ms = false;
 
 // LEDをチカチカさせるための関数
-void LEDblink(byte pin, int times, int interval){
+void LEDblink(byte pin, int times, int interval)
+{
   analogWrite(pin, 0);
-  for(int i = 0; i < times; i++){
+  for (int i = 0; i < times; i++)
+  {
     delay(interval);
     analogWrite(pin, 255);
     delay(interval);
@@ -35,75 +38,90 @@ void LEDblink(byte pin, int times, int interval){
 }
 
 // setupで有効にされるタイマ割り込み処理が書いてある場所
-void timer_warikomi(){    
+void timer_warikomi()
+{
   // RGB LED を良い感じに光らせるための処理
   static int count = 0;
   static int count_flag = 0;
   count += 2; // ここで光る周期を変えられる(はず)
   count_flag++;
 
-  if(count < 255){
+  if (count < 255)
+  {
     analogWrite(PIN_LED_RED, count);
     analogWrite(PIN_LED_BLUE, 255 - count);
-  }else if(count < 255 * 2){
+  }
+  else if (count < 255 * 2)
+  {
     analogWrite(PIN_LED_GREEN, count - 255);
     analogWrite(PIN_LED_RED, 255 * 2 - count);
-  }else if(count < 255 * 3){
+  }
+  else if (count < 255 * 3)
+  {
     analogWrite(PIN_LED_BLUE, count - 255 * 2);
     analogWrite(PIN_LED_GREEN, 255 * 3 - count);
-  }else{
+  }
+  else
+  {
     count = 0;
   }
 
   // フラグ立てるための処理
   flag_10ms = true;
-  /*
-  if(count_flag >= 10){
-    flag_100ms = true;
-    count_flag = 0;
-  }*/
-
 }
 
 void setup()
 {
   Serial.begin(115200);
-  
+
   // AMT203Vの初期化
-  SPI.begin(); // ここでSPIをbeginしてあげないとちゃんと動かなかった
+  SPI.begin();                          // ここでSPIをbeginしてあげないとちゃんと動かなかった
   SPI.setClockDivider(SPI_CLOCK_DIV16); //SPI通信のクロックを1MHzに設定 beginの後に置かないと，処理が止まる
   Serial.println(amt203_1.init());
   Serial.println(amt203_2.init());
-    
+  //ここのinitで1が出力されなかった場合はうまく読み取れてない．
+
   LEDblink(PIN_LED_GREEN, 2, 100); // 初期が終わった証拠にブリンク
   Serial.println("AMT203V init done!");
   Serial.flush();
-  
-  //amt203_1.setZeroPos();
-  //amt203_2.setZeroPos();
+
   MsTimer2::set(10, timer_warikomi); // 10ms period
   MsTimer2::start();
-  
 }
 
 void loop()
 {
   // 10msに1回ピン情報を出力する
-  if(flag_10ms){
-  
-    int thetaDuEnc_1;
-    int thetaDuEnc_2; /*,preThetaDuEnc*/;
-    thetaDuEnc_1 = amt203_1.getEncount(); 
-    //delay(10);
-    thetaDuEnc_2 = amt203_2.getEncount();
-    //delay(10);
+  if (flag_10ms)
+  {
 
+    int Enc_read_1;
+    int Enc_read_2; /*,preEnc_read*/
+    ;
+    int SW_read_1 = digitalRead(SW_1);
+    int SW_read_2 = digitalRead(SW_2);
+
+    Enc_read_1 = amt203_1.getEncount();
+    Enc_read_2 = amt203_2.getEncount();
+
+
+    //AMT203の原点をリセットする>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    if (SW_read_1 == 1)
+    {
+      amt203_1.setZeroPos();
+    }
+    else if (SW_read_2 == 1)
+    {
+      amt203_2.setZeroPos();
+    }
+    //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+    //読み取った値をtera termで表示する
     Serial.print("ENC1_");
-    Serial.print(thetaDuEnc_1);
+    Serial.print(Enc_read_1);
     Serial.print("___ENC2_");
-    Serial.println(thetaDuEnc_2);
-    
+    Serial.println(Enc_read_2);
+
     flag_10ms = false;
-    
   }
 }
--- AMT203/define.h
+++ AMT203/define.h
@@ -5,6 +5,11 @@
 
 #define PIN_CSB1 8
 #define PIN_CSB2 9
+//AMT203のCSBピンは，68,69,70番は使用できない．
+//他にも使用できないピンがある可能性有，読めない場合はピンを変えてみること．
+
+#define SW_1 1
+#define SW_2 2
 
 // 制御周期
 #define INT_TIME			( 0.01 )//( 0.001 )
